8f699df618d24713c3754cf89a888ae0c1a996f9
junior brandao <edilson_brandaojunior@hotmail.com>
2023-10-20 20:00:20 -0300

    add knex

--- src/conexao.js
+++ src/conexao.js
@@ -3,11 +3,11 @@
 const knex = require('knex')({
    client: 'pg',
    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: 'ec2-44-215-40-87.compute-1.amazonaws.com',
+      port: 5432,
+      user: 'dikseszuvmfdya',
+      password: '7ace02179fd0ee0177481095c3f1b69f598fe96ac23ab35fa43f062283a7be51',
+      database: 'dde9uno9tbq4ga',
    },
    ssl: { rejectUnauthorized: false },
 })
