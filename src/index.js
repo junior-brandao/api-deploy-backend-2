6b566866d69870f0d42cdcf40de0504b3556cf02
junior brandao <edilson_brandaojunior@hotmail.com>
2023-10-20 21:09:37 -0300

    config cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const cors = require('cors')
 
 require('dotenv').config({
    path: './.env',
@@ -7,6 +8,7 @@ require('dotenv').config({
 
 const app = express()
 
+app.use(cors)
 app.use(express.json())
 
 const knex = require('./conexao')
